46876b3345e339367ede958da79829aa8410550e
semih <semih@uraltelekom.com>
2023-06-21 16:19:02 +0300

    * Add api healtCheck endpoint

--- api/index.js
+++ api/index.js
@@ -87,6 +87,9 @@ app.use('/api/user', userRoute)
 app.use('/api/website', websiteRoute)
 app.use('/api/page', pageRoute);
 app.use('/api/review', reviewRoute);
+app.use('/api/healtCheck', (req, res) => {
+  res.status(200).json({ message: 'Server is running!' });
+});
 app.use('*', (request, response) => {
   response.status(404).json({ message: 'Route not found!' });
 });
